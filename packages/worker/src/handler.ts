4bdabe5aef80f392bd360d977bfbd3381e721f5c
invidia <86919879+SyInvidia@users.noreply.github.com>
2021-08-28 15:50:25 +0100

    Update handler.ts

--- packages/worker/src/handler.ts
+++ packages/worker/src/handler.ts
@@ -1,3 +1,15 @@
 export async function handleRequest(request: Request): Promise<Response> {
   return new Response(`request method: ${request.method}`)
 }
+
+const API_KEY = 'b3cf80e6-0906-45c4-aee0-5d63b48345eb'
+const URL = 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion?amount=1&symbol=MIA&convert=STX&CMC_PRO_API_KEY=' + API_KEY;
+
+async function getMIAPriceInSTX() {
+  const res = await fetch(URL);
+  const result = await res.json();
+  console.log('RESULT OBJ: ' + result);
+  const price = result.data.quote.STX.price;
+  console.log('PRICE OF MIA IN STX: ' + price);
+  return price;
+}
